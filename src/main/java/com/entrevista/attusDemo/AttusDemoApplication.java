263689ba63d332472d33dc4415fb6de32f1eb45f
Breno Kluck <brenokluck@outlook.com>
2026-06-19 08:56:52 -0300

    first commit

---
+++ src/main/java/com/entrevista/attusDemo/AttusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.entrevista.attusDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entrevista/attusDemo/AttusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrevista.attusDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
